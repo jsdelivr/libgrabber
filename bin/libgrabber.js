d097173ced1b3804f2854ddd5955de7e015b98e0
Aleksandar Antok <aleksandara@gmail.com>
2014-04-10 14:06:53 +0200

    Rename config options

--- bin/libgrabber.js
+++ bin/libgrabber.js
@@ -5,8 +5,8 @@ var path = require('path');
 var config = require('../lib/config');
 var grabber = require('../lib/libgrabber');
 
-var jsDelivrPath = config.get('jsDelivrPath');
-var projectsDir = config.get('jsDelivrProjectsDir');
+var jsDelivrPath = config.get('jsdelivr-path');
+var projectsDir = config.get('jsdelivr-projects-dir');
 var projectsPath = path.join(jsDelivrPath, projectsDir);
 
 grabber.updateAll(jsDelivrPath, projectsPath);
